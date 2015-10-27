f2f0481d695dd5a7da7a381b61f7170859eff6cf
Gonzalo <zalo250@gmail.com>
2015-10-27 16:02:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 1337;
 var cors_proxy = require('cors-anywhere');
 
 cors_proxy.createServer({
-    originWhitelist: (process.env.PORT ? ['http://www.gonzalobarba.com'] : []),
+    originWhitelist: [],
     requireHeader: [],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function () {
